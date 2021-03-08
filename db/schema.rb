51cddc3f477380c0fdfa5e3435f3d315929d7666
Abhishek Sharma <abhishek.sharma@bluebash.co>
2021-03-08 16:42:51 +0530

    Changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_092154) do
+ActiveRecord::Schema.define(version: 2021_03_05_092139) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,15 +50,6 @@ ActiveRecord::Schema.define(version: 2021_03_05_092154) do
     t.index ["user_id"], name: "index_albums_on_user_id"
   end
 
-  create_table "taggings", force: :cascade do |t|
-    t.integer "album_id", null: false
-    t.integer "tag_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["album_id"], name: "index_taggings_on_album_id"
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
-  end
-
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -79,6 +70,4 @@ ActiveRecord::Schema.define(version: 2021_03_05_092154) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "taggings", "albums"
-  add_foreign_key "taggings", "tags"
 end
