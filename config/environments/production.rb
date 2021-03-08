f38d7aaa2c71e5167451f966a34dfce2fe130b77
Abhishek Sharma <abhishek.sharma@bluebash.co>
2021-03-08 15:46:13 +0530

    Forget Password Functionality added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,9 +99,9 @@ Rails.application.configure do
 
 
   
-  config.action_mailer.default_url_options = { host: 'railsalbum.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'railsalbum.herokuapp.com' }
 
-config.action_mailer.delivery_method = :smtp
+
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
