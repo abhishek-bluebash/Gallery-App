5a34dd6be0a6622d895ea8df1591fe837db3014b
Abhishek Sharma <abhishek.sharma@bluebash.co>
2021-03-08 15:25:12 +0530

    Forget Password Functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :albums
   root 'albums#index'
+  # get "password/reset", to: "password_resets#new"
+  # post "password/reset", to: "password_resets#create"
   devise_for :users
   resources :tag
   resources :albums do
